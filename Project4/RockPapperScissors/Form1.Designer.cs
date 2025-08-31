11a71a3d905ab84fd34b2274785ce3e2b69a9d12
KarimaFathi <karimafathifadili@gmail.com>
2025-08-31 16:44:14 +0100

    set up WinForms project structure and form layouts

---
+++ Project4/RockPapperScissors/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace RockPapperScissors
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::RockPapperScissors.Properties.Resources.rockPaperScissors;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1357, 663);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnStart.Font = new System.Drawing.Font("Swis721 Blk BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btnStart.Location = new System.Drawing.Point(535, 482);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(242, 79);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start Game";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.AliceBlue;
+            this.label1.Font = new System.Drawing.Font("Swis721 BlkEx BT", 20F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label1.Location = new System.Drawing.Point(281, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(773, 48);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Rock Paper Scissor Game";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1352, 664);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Project4/RockPapperScissors/Form2.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace RockPapperScissors
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbWinner = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.rdRock = new System.Windows.Forms.RadioButton();
+            this.rdPaper = new System.Windows.Forms.RadioButton();
+            this.rdScissor = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SkyBlue;
+            this.panel1.Controls.Add(this.btnClose);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.lbWinner);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(474, 664);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Swis721 Blk BT", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(420, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "How many rounds you want to play ?";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Azure;
+            this.panel2.Location = new System.Drawing.Point(454, 1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(899, 664);
+            this.panel2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(123, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(176, 30);
+            this.textBox1.TabIndex = 1;
+            // 
+            // lbWinner
+            // 
+            this.lbWinner.AutoSize = true;
+            this.lbWinner.Font = new System.Drawing.Font("Swis721 Blk BT", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbWinner.ForeColor = System.Drawing.Color.Gold;
+            this.lbWinner.Location = new System.Drawing.Point(131, 176);
+            this.lbWinner.Name = "lbWinner";
+            this.lbWinner.Size = new System.Drawing.Size(168, 44);
+            this.lbWinner.TabIndex = 2;
+            this.lbWinner.Text = "Winner ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Swis721 Blk BT", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label2.Location = new System.Drawing.Point(101, 263);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(220, 39);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "In Progress";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Azure;
+            this.panel3.Controls.Add(this.rdScissor);
+            this.panel3.Controls.Add(this.rdPaper);
+            this.panel3.Controls.Add(this.rdRock);
+            this.panel3.ForeColor = System.Drawing.SystemColors.Control;
+            this.panel3.Location = new System.Drawing.Point(0, 333);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(461, 90);
+            this.panel3.TabIndex = 4;
+            // 
+            // rdRock
+            // 
+            this.rdRock.AutoSize = true;
+            this.rdRock.Font = new System.Drawing.Font("Swis721 Blk BT", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdRock.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.rdRock.Location = new System.Drawing.Point(29, 33);
+            this.rdRock.Name = "rdRock";
+            this.rdRock.Size = new System.Drawing.Size(96, 28);
+            this.rdRock.TabIndex = 0;
+            this.rdRock.TabStop = true;
+            this.rdRock.Text = " Rock";
+            this.rdRock.UseVisualStyleBackColor = true;
+            // 
+            // rdPaper
+            // 
+            this.rdPaper.AutoSize = true;
+            this.rdPaper.Font = new System.Drawing.Font("Swis721 Blk BT", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdPaper.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.rdPaper.Location = new System.Drawing.Point(163, 33);
+            this.rdPaper.Name = "rdPaper";
+            this.rdPaper.Size = new System.Drawing.Size(100, 28);
+            this.rdPaper.TabIndex = 1;
+            this.rdPaper.TabStop = true;
+            this.rdPaper.Text = "Paper";
+            this.rdPaper.UseVisualStyleBackColor = true;
+            // 
+            // rdScissor
+            // 
+            this.rdScissor.AutoSize = true;
+            this.rdScissor.Font = new System.Drawing.Font("Swis721 Blk BT", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdScissor.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.rdScissor.Location = new System.Drawing.Point(297, 33);
+            this.rdScissor.Name = "rdScissor";
+            this.rdScissor.Size = new System.Drawing.Size(115, 28);
+            this.rdScissor.TabIndex = 2;
+            this.rdScissor.TabStop = true;
+            this.rdScissor.Text = "Scissor";
+            this.rdScissor.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button1.Font = new System.Drawing.Font("Swis721 Blk BT", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Azure;
+            this.button1.Location = new System.Drawing.Point(14, 474);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 58);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Start ";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button2.Font = new System.Drawing.Font("Swis721 Blk BT", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Azure;
+            this.button2.Location = new System.Drawing.Point(217, 474);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(203, 58);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Restart Game";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btnClose.Font = new System.Drawing.Font("Swis721 Blk BT", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.Azure;
+            this.btnClose.Location = new System.Drawing.Point(113, 569);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(197, 58);
+            this.btnClose.TabIndex = 7;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1352, 664);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lbWinner;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton rdScissor;
+        private System.Windows.Forms.RadioButton rdPaper;
+        private System.Windows.Forms.RadioButton rdRock;
+    }
+}
